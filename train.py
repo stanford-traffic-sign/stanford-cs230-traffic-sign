62ca1b82e5d7ffca023a6efb1390aec819038561
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-27 19:13:48 +0800

    feat: add train

---
+++ train.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn, optim
+from model.net import Net
+
+
+def train(model, optimizer, loss_fn, train_data_loader):
+    for epoch in range(2):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(train_data_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 200 == 199:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 200))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    PATH = './my_net.pth'
+    torch.save(net.state_dict(), PATH)
+
+
+if __name__ == '__main__':
+    model = Net()
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    train(model, optimizer, loss_fn, train_data_loader)
