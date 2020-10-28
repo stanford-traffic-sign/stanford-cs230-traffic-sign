127320dd5aadfa1b832aa592ea660d3c6a33a9e0
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-28 18:26:58 +0800

    feat: add more classes

--- utils/data_class.py
+++ utils/data_class.py
@@ -1,2 +1,51 @@
-class_names = ['speed_30', 'priority_road', 'give_way', 'stop', 'no_entry', 'left_straight', 'roundabout']
-class_indices = [1, 12, 13, 14, 17, 37, 40]
+class_names = [
+    # prohibitory
+    'speed_30',
+    'speed_120',
+    'no_overtaking',
+    'prohibition',
+
+    # danger
+    'construction',
+    'signal_ahead',
+    'pedestrian',
+    'deer',
+
+    # mandatory
+    'right',
+    'straight',
+    # 'left_straight',
+    # 'roundabout',
+
+    # others
+    # 'priority_road',
+    # 'give_way',
+    # 'stop',
+    # 'cancel_no_overtaking',
+]
+
+class_indices = [
+    # prohibitory
+    1,  # speed_30
+    8,  # speed_120
+    9,  # no_overtaking
+    15,  # prohibition
+
+    # danger
+    25,  # construction
+    26,  # signal_ahead
+    27,  # pedestrian
+    31,  # deer
+
+    # mandatory
+    33,  # right
+    35,  # straight
+    # 37,  # left_straight
+    # 40,  # roundabout
+
+    # others
+    # 12,  # priority_road
+    # 13,  # give_way
+    # 14,  # stop
+    # 41,  # cancel_no_overtaking
+]
