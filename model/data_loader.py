ed08de06bd2550e26f235eca989129881ec55d13
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-29 23:04:07 +0800

    perf: use mean and std values from ImageNet

--- model/data_loader.py
+++ model/data_loader.py
@@ -7,17 +7,20 @@ BATCH_SIZE = 64
 TRAIN_DATA_PATH = './data/train/'
 VAL_DATA_PATH = './data/val/'
 
+mean_nums = [0.485, 0.456, 0.406]
+std_nums = [0.229, 0.224, 0.225]
+
 train_transform = transforms.Compose([
     transforms.Resize((32, 32)),
     transforms.RandomRotation(degrees=15),
     transforms.ColorJitter(0.4, 0.4, 0.4),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize(mean=mean_nums,  std=std_nums),
 ])
 val_transform = transforms.Compose([
     transforms.Resize((32, 32)),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize(mean=mean_nums,  std=std_nums),
 ])
 
 train_data = ImageFolder(root=TRAIN_DATA_PATH, transform=train_transform)
