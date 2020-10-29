ae644a4b48378d2ca7b4e166eafc0e0ed129b6e4
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-29 22:27:21 +0800

    feat: add batch normalization

--- model/net.py
+++ model/net.py
@@ -9,15 +9,16 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)  # in_channels, out_channels, kernel_size
         self.pool = nn.MaxPool2d(2, stride=2)  # kernel_size
+        self.bn1 = nn.BatchNorm2d(6)
         self.conv2 = nn.Conv2d(6, 16, 5)
+        self.bn2 = nn.BatchNorm2d(16)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)  # in_features, out_features
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 16)
         self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.bn1(self.pool(F.relu(self.conv1(x))))
+        x = self.bn2(self.pool(F.relu(self.conv2(x))))
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
