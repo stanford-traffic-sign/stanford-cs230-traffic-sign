069709487ef2582daa92a5736fa2a672d69d5563
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-01 07:31:40 +0800

    feat(chart): add title and x label

--- visualize.py
+++ visualize.py
@@ -8,5 +8,7 @@ import config
 if __name__ == '__main__':
     statistics_data = pd.read_json(config.statistics_path)
     plt.figure(figsize=(16, 6))
+    plt.title('Accuracy and Loss During the Train')
+    plt.xlabel('Epoch')
     sns.lineplot(data=statistics_data)
     plt.show()
