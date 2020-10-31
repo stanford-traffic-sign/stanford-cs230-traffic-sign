07afdae9868a42ea20b16f5d1918ac26e9748227
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-10-31 08:01:01 +0800

    chore: change transforms.ColorJitter to 0.5, 0.5, 0.5, 0.1

--- model/data_loader.py
+++ model/data_loader.py
@@ -13,7 +13,7 @@ std_nums = [0.229, 0.224, 0.225]
 train_transform = transforms.Compose([
     transforms.RandomResizedCrop(size=32, scale=(0.9, 1.1), ratio=(0.75, 1.33)),
     transforms.RandomRotation(degrees=15),
-    transforms.ColorJitter(0.4, 0.4, 0.4, 0.1),
+    transforms.ColorJitter(0.5, 0.5, 0.5, 0.1),
     transforms.ToTensor(),
     transforms.Normalize(mean=mean_nums,  std=std_nums),
 ])
