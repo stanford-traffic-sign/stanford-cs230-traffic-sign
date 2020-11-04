773d8fe755ab985b69da53699cb353f705bbbbc2
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-04 19:48:36 +0800

    feat: add confusion matrix

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,8 @@
 from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import torch
 
 from model import net
@@ -52,15 +55,39 @@ def get_predictions(model, data_loader):
     return real_values, predictions
 
 
+def show_confusion_matrix(confusion_matrix, class_names):
+    cm = confusion_matrix.copy()
+
+    cell_counts = cm.flatten()
+
+    cm_row_norm = cm / cm.sum(axis=1)[:, np.newaxis]
+
+    row_percentages = ["{0:.2f}".format(value) for value in cm_row_norm.flatten()]
+
+    cell_labels = [f"{cnt}\n{per}" for cnt, per in zip(cell_counts, row_percentages)]
+    cell_labels = np.asarray(cell_labels).reshape(cm.shape[0], cm.shape[1])
+
+    df_cm = pd.DataFrame(cm_row_norm, index=class_names, columns=class_names)
+
+    hmap = sns.heatmap(df_cm, annot=cell_labels, fmt="", cmap="Blues")
+    hmap.yaxis.set_ticklabels(hmap.yaxis.get_ticklabels(), rotation=0, ha='right')
+    hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation=30, ha='right')
+    plt.ylabel('True Sign')
+    plt.xlabel('Predicted Sign')
+
+
 if __name__ == '__main__':
     model = net.Net()
     model.to(device)
 
     model.load_state_dict(torch.load(config.model_path))
-
     val_accuracy, _, num_inputs = evaluate(model, net.loss_fn, val_data_loader, device)
 
     print(f'Accuracy {round(val_accuracy * 100, 2)}% ({num_inputs} images)')
 
     y_true, y_pred = get_predictions(model, val_data_loader)
     print(classification_report(y_true, y_pred, target_names=class_map.values(), digits=4))
+
+    # cm = confusion_matrix(y_true, y_pred)
+    # show_confusion_matrix(cm, class_map.values())
+    # plt.show()
