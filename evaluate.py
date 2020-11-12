f05d441cca1f56dd7eb305230165c901142789c2
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-12 23:32:31 +0800

    chore: change " to '

--- evaluate.py
+++ evaluate.py
@@ -58,19 +58,16 @@ def get_predictions(net, data_loader):
 
 def show_confusion_matrix(confusion_matrix, class_names):
     cm = confusion_matrix.copy()
-
     cell_counts = cm.flatten()
-
     cm_row_norm = cm / cm.sum(axis=1)[:, np.newaxis]
+    row_percentages = ['{0:.2f}'.format(value) for value in cm_row_norm.flatten()]
 
-    row_percentages = ["{0:.2f}".format(value) for value in cm_row_norm.flatten()]
-
-    cell_labels = [f"{cnt}\n{per}" for cnt, per in zip(cell_counts, row_percentages)]
+    cell_labels = [f'{cnt}\n{per}' for cnt, per in zip(cell_counts, row_percentages)]
     cell_labels = np.asarray(cell_labels).reshape(cm.shape[0], cm.shape[1])
 
     df_cm = pd.DataFrame(cm_row_norm, index=class_names, columns=class_names)
 
-    hmap = sns.heatmap(df_cm, annot=cell_labels, fmt="", cmap="Blues")
+    hmap = sns.heatmap(df_cm, annot=cell_labels, fmt='', cmap='Blues')
     hmap.yaxis.set_ticklabels(hmap.yaxis.get_ticklabels(), rotation=0, ha='right')
     hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation=30, ha='right')
     plt.ylabel('True Sign')
@@ -82,7 +79,6 @@ if __name__ == '__main__':
 
     net.load_state_dict(torch.load(config.model_path))
     val_accuracy, _, num_inputs = evaluate(net, loss_fn, val_data_loader, device)
-
     print(f'Accuracy {round(val_accuracy * 100, 2)}% ({num_inputs} images)')
 
     y_true, y_pred = get_predictions(net, val_data_loader)
--- utils/tensorboard.py
+++ utils/tensorboard.py
@@ -12,7 +12,7 @@ def matplotlib_imshow(img, one_channel=False):
     img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     if one_channel:
-        plt.imshow(npimg, cmap="Greys")
+        plt.imshow(npimg, cmap='Greys')
     else:
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
@@ -23,7 +23,7 @@ def images_to_probs(net, images):
     network and a list of images
     """
     output = net(images)
-    # convert output probabilities to predicted class
+    # Convert output probabilities to predicted class
     _, preds_tensor = torch.max(output, 1)
     preds = np.squeeze(preds_tensor.numpy())
     return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
@@ -35,22 +35,17 @@ def plot_classes_preds(net, images, labels):
     and labels from a batch, that shows the network's top prediction along
     with its probability, alongside the actual label, coloring this
     information based on whether the prediction was correct or not.
-    Uses the "images_to_probs" function.
+    Uses the 'images_to_probs' function.
     """
     preds, probs = images_to_probs(net, images)
-    # print('preds', preds)
-    # print('probs', probs)
-    # plot the images in the batch, along with predicted and true labels
+    # Plot the images in the batch, along with predicted and true labels
     fig = plt.figure(figsize=(12, 48))
     for i in np.arange(4):
         ax = fig.add_subplot(1, 4, i + 1, xticks=[], yticks=[])
         matplotlib_imshow(images[i], one_channel=True)
-        # print('labels', labels)
-        # print('labels[i]', labels[i])
-        # print('class_map[labels[i]]', class_map[labels[i]])
-        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+        ax.set_title('{0}, {1:.1f}%\n(label: {2})'.format(
             class_map[preds[i]],
             probs[i] * 100.0,
             class_map[labels[i].item()]),
-            color=("green" if preds[i] == labels[i].item() else "red"))
+            color=('green' if preds[i] == labels[i].item() else 'red'))
     return fig
