323ea02f13c195b804f375883b692337fbe890cd
Hongbo Miao <Hongbo.Miao@outlook.com>
2020-11-14 16:28:46 +0800

    feat: load latest model for PR curves

--- pr_curves.py
+++ pr_curves.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 
 from utils.data_class import num_classes, class_map
 from utils.writer import writer
+import config
 
 
 def add_pr_curve_tensorboard(writer, class_map, class_index, test_probs, test_preds, global_step=0):
@@ -19,10 +20,13 @@ def add_pr_curve_tensorboard(writer, class_map, class_index, test_probs, test_pr
 
 
 if __name__ == '__main__':
+    net.load_state_dict(torch.load(config.model_path))
+
     # 1. Get the probability predictions in a test_size x num_classes Tensor
     # 2. Get the preds in a test_size Tensor
     class_probs = []
     class_preds = []
+
     with torch.no_grad():
         for data in val_data_loader:
             images, labels = data
